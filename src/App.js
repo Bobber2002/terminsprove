3f8b5cf756abf6ff2da6608da7928746fe4be242
ChristofferRTS <4906240@RTS-365.dk>
2022-09-03 21:19:12 +0200

    update pages/routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
 import BgWrapper from "./components/background/backgroundWrapper";
 import WelcomePage from "./pages/welcome/welcomePage";
 import HomePage from "./pages/home/homePage";
+import Navigation from "./components/navigtaion/navigation";
+import SearchPage from "./pages/search/searchPage";
+import SchedulePage from "./pages/schedule/schedulePage";
+import ClassPage from "./pages/class/classPage";
 
 function App() {
+  const [navState, setNavState] = useState(false);
+
+  useEffect(() => {
+    console.log(navState);
+  }, [navState]);
+
   return (
     <div className="App">
       <BgWrapper>
-        <div className="absolute flex justify-between w-[375px] px-6 pt-2">
-        <p>9:41</p>
-        <p>xxx</p>
-        </div>
         <Router>
-          <Routes>
-            <Route path="/" element={<WelcomePage/>} />
-            <Route path="/home" element={<HomePage/>} />
-          </Routes>
+          <div className="absolute z-10 flex justify-between w-[375px] px-6 pt-2">
+            <p>9:41</p>
+            <p>xxx</p>
+          </div>
+          {navState ? (
+            <Navigation navState={navState} setNavState={setNavState} />
+          ) : (
+            <Routes>
+              <Route path="/" element={<WelcomePage />} />
+              <Route
+                path="/home"
+                element={
+                  <HomePage navState={navState} setNavState={setNavState} />
+                }
+              />
+              <Route
+                path="/search"
+                element={
+                  <SearchPage navState={navState} setNavState={setNavState} />
+                }
+              />
+              <Route
+                path="/schedule"
+                element={
+                  <SchedulePage navState={navState} setNavState={setNavState} />
+                }
+              />
+              <Route
+                path="/class"
+                element={
+                  <ClassPage navState={navState} setNavState={setNavState} />
+                }
+              />
+            </Routes>
+          )}
         </Router>
       </BgWrapper>
     </div>
